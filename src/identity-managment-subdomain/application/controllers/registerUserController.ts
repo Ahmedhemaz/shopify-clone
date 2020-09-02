4593cd32534286c97033223b2d395dcdbae82922
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-09-02 05:53:00 +0200

    Create registerUsercontroller, validators dirs

---
+++ src/identity-managment-subdomain/application/controllers/registerUserController.ts
@@ -0,0 +1,8 @@
+export class RegisterUserController {
+    // create user entity
+    // create user repository
+    // presist user data
+    // emit registered event
+    // send confirmation mail
+    
+}
