a85f109c42fd8ec189bdfb45d32bb3b9981f9c4e
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-09-08 16:13:07 +0200

    move usermapper to persistance

---
+++ src/identity-managment-subdomain/infrastructrue/persistance/mappers/user.mapper.ts
@@ -0,0 +1,58 @@
+import 'reflect-metadata'
+import { IDataModelMapper } from "../../../../shared-kernal/interfaces/IDataModelMapper";
+import { User } from "../../../domain/user.entity";
+import { UserDataModel } from "../models/UserDataModel";
+import { injectable, inject } from "inversify";
+import { TYPES } from "../../../../shared-kernal/ioc/types";
+import { IHashService } from "../../../domain/interfaces/services/hashService.interface";
+import { FullName } from "../../../domain/value-objects/name.valueObject";
+import { FullNameDataModel } from "../models/FullNameDataModel";
+import { Address } from "../../../domain/value-objects/address.valueObject";
+import { AddressDataModel } from "../models/AddressDataModel";
+
+@injectable()
+export class UserDataMapper implements IDataModelMapper<User, UserDataModel> {
+
+    private hashingService: IHashService;
+    constructor(@inject(TYPES.IHashService) hashingService: IHashService) {
+        this.hashingService = hashingService;
+    }
+
+    public async mapDomainModelToDataModel(domainModel: User): Promise<UserDataModel> {
+        const userDataModel: UserDataModel = new UserDataModel();
+        userDataModel.uId = domainModel.getUId();
+        userDataModel.name = this.mapDomainModelFullNameToDataModelFullName(domainModel.getFullName());
+        userDataModel.email = domainModel.getEmailAddress().getEmail();
+        userDataModel.mobileNumber = domainModel.getMobile().getNumber();
+        userDataModel.address = this.mapDomainModelAddressToDataModelAddress(domainModel.getAddress());
+        userDataModel.hashedPassword = await this.hashingService.hashPassword(domainModel.getPassword().getPassword());
+        return userDataModel;
+    }
+
+    public mapDataModelToDomainModel(dataModel: UserDataModel): User {
+        return new User.userBuilder().setUniqueId(dataModel.uId)
+            .setName(dataModel.name.firstName, dataModel.name.lastName)
+            .setEmail(dataModel.email)
+            .setMobile(dataModel.mobileNumber)
+            .setAddress(dataModel.address.country, dataModel.address.city,
+                dataModel.address.street, dataModel.address.postalCode)
+            .build();
+    }
+
+    private mapDomainModelFullNameToDataModelFullName(domainModelFullName: FullName): FullNameDataModel {
+        const fullNameDataModel = new FullNameDataModel();
+        fullNameDataModel.firstName = domainModelFullName.getFirstName();
+        fullNameDataModel.lastName = domainModelFullName.getLastName();
+        return fullNameDataModel;
+    }
+
+    private mapDomainModelAddressToDataModelAddress(domainModelAddress: Address): AddressDataModel {
+        const addressDataModel = new AddressDataModel();
+        addressDataModel.country = domainModelAddress.getCountry();
+        addressDataModel.city = domainModelAddress.getCity();
+        addressDataModel.street = domainModelAddress.getStreet();
+        addressDataModel.postalCode = domainModelAddress.getPostalCode();
+        return addressDataModel;
+    }
+
+}
